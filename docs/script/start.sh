e2b26d325a3e8912480c2c799f573ba468133768
yangyong <398901650@qq.com>
2015-12-05 20:14:23 +0800

    编写启动脚本

--- docs/script/start.sh
+++ docs/script/start.sh
@@ -1,3 +1,5 @@
-spark-submit --jars /opt/apps/page_rank/libs/mysql-connector-java-5.1.17.jar  --class com.jetyun.newswire.main.BootStrap --master yarn-cluster /opt/apps/page_rank/libs/outguard-newswire_2.10-1.0.jar yarn-cluster 1000
+spark-submit --jars /opt/apps/page_rank/libs/mysql-connector-java-5.1.17.jar  --class com.jetyun.newswire.main.BootStrap --master yarn-cluster /opt/apps/page_rank/libs/outguard-newswire_2.10-1.0.jar yarn-cluster 1000 /user/root/pagerank/model/tfidf.model
 
 mysqldump -B outguard_crawler --ignore-table=outguard_crawler.webpage -h rds5hhm9neufn4xn2mix.mysql.rds.aliyuncs.com -ucrawler -pR5mTSEUJLZtwLbGf -r db1.sql
+
+mysql -h rds5hhm9neufn4xn2mix.mysql.rds.aliyuncs.com -ucrawler -pR5mTSEUJLZtwLbGf 
